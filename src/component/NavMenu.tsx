eaa6ac721a58a727ffe17c98227c4e7b7d88ed0d
dggrunzweig <dggrunzweig@gmail.com>
2024-10-29 14:57:27 -0700

    mobile nav menu

--- src/component/NavMenu.tsx
+++ src/component/NavMenu.tsx
@@ -44,6 +44,34 @@ const NavMenu = ({ menu_item_names, onItemClick, current_page }: props) => {
           &#169; 2024
         </div>
       </div>
+      <div className="nav-menu-mobile">
+        <div
+          className={open_menu ? "small-button-inactive" : "small-button"}
+          onClick={() => {
+            setOpenMenu(!open_menu);
+          }}
+        >
+          Menu
+        </div>
+        {open_menu &&
+          menu_item_names.map((m, i) => {
+            return (
+              <div className="menu-item" key={m}>
+                {i == current_page && <div className="page-indicator"></div>}
+                <div
+                  className="small-button"
+                  onClick={() => {
+                    onItemClick(i);
+                    document.body.scrollTo({ top: 0, behavior: "instant" });
+                    setOpenMenu(false);
+                  }}
+                >
+                  {m}
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 };
