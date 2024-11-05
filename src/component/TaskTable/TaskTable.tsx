61fd0775a7c9d5814f8de34f05e8718da8a7a7dd
dggrunzweig <dggrunzweig@gmail.com>
2024-11-04 18:57:55 -0800

    fix build errors

--- src/component/TaskTable/TaskTable.tsx
+++ src/component/TaskTable/TaskTable.tsx
@@ -1,4 +1,4 @@
-import { useId, useState } from "react";
+import { useState } from "react";
 import "./TaskTable.css";
 import TaskTableAddEdit from "./TaskTableAddEdit";
 import TableRow from "./TableRow";
